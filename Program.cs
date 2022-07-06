bda0357ee0b26af7bbc6c2d388cc61968b22d417
MortyMels <morty.mels@gmail.com>
2022-07-06 19:37:33 +0300

    dz00.01.001

--- Program.cs
+++ Program.cs
@@ -1,25 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int? max = default(int);
+int? max = null;
 int num;
-//max = 0;
+Random rnd = new Random();
 bool r = true;
 while(r){
-    Console.Write("Введите число или max для определения максимального числа: ");
+    Console.Write("Введите число или rnd");
+
+    if (max is not null){
+        Console.Write(" или max: ");
+    }else{
+        Console.Write(": ");
+    }
+
     string str = Console.ReadLine();
 
     if (int.TryParse(str, out num)){
         if(max == null){
-            max=;
-        }else{
-            max=Math.Max(Convert.ToInt32(max),num);
+            max=num;
         }
+        max=Math.Max(Convert.ToInt32(max),num);
     }else if(str=="max"){
-         r=false;
-    }else if(){
-    
+        if(max == null){
+            Console.Write("Ни одно число не введено - ");
+        }else{
+            r=false;
+        }
+    }else if (str=="rnd"){
+        num = rnd.Next();
+        Console.WriteLine($"Сгенерировано число: {num}");
+        
+        if(max is null){
+            max=num;
+        }
+        max=Math.Max(Convert.ToInt32(max),num);
     }else{
         Console.WriteLine("Введено неверное значение, попробуйте еще раз");
     }
 }
-Console.WriteLine($"Максимальное число{max}");
+Console.WriteLine($"Максимальное число: {max}");
 
